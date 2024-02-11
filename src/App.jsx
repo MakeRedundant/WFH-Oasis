04a353a24ad632d3907d2223313a78889c03486c
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2024-02-11 22:24:17 +1100

    added router links to shop pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
 import './App.css';
-import "./scss/main.scss";
+import './scss/main.scss';
 import Homepage from './pages/homepage/homepage';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
-// import { Switch, Route, withRouter, Redirect } from "react-router-dom";
-// import Header from "./components/header/header.component";
-// import LogIn from "./pages/Log-in/log-in.component";
-// import SignUp from "./pages/sign-up/sign-up.component";
-// import Everything from "./pages/shop/everything.component";
-// import Chairs from "./pages/shop/chairs.component";
-// import Sofas from "./pages/shop/sofas.component";
-// import Stools from "./pages/shop/stools.components";
-// import Lights from "./pages/shop/lights.component";
-// import Ottomans from "./pages/shop/ottomans.component";
-// import Checkout from "./pages/checkout/checkout.component";
-// import Footer from "./components/footer/footer.component";
-// import { connect } from "react-redux";
-// import { createStructuredSelector } from "reselect";
 
+import Everything from "./pages/shop/everything.component";
+import Chairs from "./pages/shop/chairs.component";
+import Sofas from "./pages/shop/sofas.component";
+import Stools from "./pages/shop/stools.components";
+import Lights from "./pages/shop/lights.component";
+import Ottomans from "./pages/shop/ottomans.component";
+import Checkout from "./pages/checkout/checkout.component";
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
-        <Homepage />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/collections" element={<Everything />} />
+          <Route path="/category/chairs" element={<Chairs />} />
+          <Route path="/category/lights" element={<Lights />} />
+          <Route path="/category/sofas" element={<Sofas />} />
+          <Route path="/category/stools" element={<Stools />} />
+          <Route path="/category/ottomans" element={<Ottomans />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="*" element={<Homepage />} />
+        </Routes>
       </Router>
     </Provider>
   );
--- src/components/collections-preview/collections-preview.component.jsx
+++ src/components/collections-preview/collections-preview.component.jsx
@@ -35,7 +35,68 @@ const CollectionPreview = ({ collections }) => {
             style={{ left: "50%", top: "12%", opacity: "1" }}
             id="1"
           />
+                      <img
+              alt=""
+              src={imgLight.third}
+              className="img__large"
+              style={{
+                top: "5%",
+                right: "50%",
+                transform: "translateX(50%)",
+                opacity: "0",
+              }}
+              id="2"
+            />
+            <img
+              alt=""
+              src={imgSofa.first}
+              className="img__large"
+              style={{ left: "45%", bottom: "10%", opacity: "0" }}
+              id="3"
+            />
+            <img
+              alt=""
+              src={imgSofa.second}
+              className="img__large"
+              style={{ right: "45%", top: "-10%", opacity: "0" }}
+              id="3"
+            />
+            <img
+              alt=""
+              src={imgChair.third}
+              className="img__small"
+              style={{ bottom: "20%", left: "20%", opacity: "1" }}
+              id="1"
+            />
+            <img
+              alt=""
+              src={imgStool.third}
+              className="img__large"
+              style={{
+                top: "5%",
+                right: "50%",
+                transform: "translateX(50%)",
+                opacity: "0",
+              }}
+              id="4"
+            />
+            <img
+              alt=""
+              src={imgOtto.third}
+              className="img__small"
+              style={{ bottom: "20%", left: "20%", opacity: "0" }}
+              id="5"
+            />
+            <img
+              alt=""
+              src={imgOtto.second}
+              className="img__small"
+              style={{ left: "50%", top: "12%", opacity: "0" }}
+              id="5"
+            />
         </div>
+
+        
 
         <div className="preview__text">
           {collections.map(({ id, urlTitle }) => (
