7cec4fa8df78d9f465895e5d5f177fc21df52c7f
MakeRedundant <brian.trang9@gmail.com>
2024-02-16 23:49:59 +1100

    still not working

--- src/components/shop-item/shop-item.component.jsx
+++ src/components/shop-item/shop-item.component.jsx
@@ -1,13 +1,63 @@
+// import React from "react";
+// import { connect } from "react-redux";
+// import "./shop-item.styles.scss";
+
+// import { addItem } from "../../redux/cart/cart.actions";
+// import { toggleModalHidden } from "../../redux/product-modal/product-modal.actions";
+// import { AddedToCartModal } from "../../animations/animations";
+// import ProductModal from "../product-modal/product-modal.component";
+
+// const ShopItem = ({ item, category, addItem, toggleModalHidden }) => {
+//   const { imageUrl, name, price } = item;
+
+//   const handleItemClick = (e) => {
+//     e.stopPropagation();
+//     console.log("Clicked item:", item);
+//     toggleModalHidden(item);
+//   };
+
+//   console.log("Props in ShopItem:", item);
+
+//   return (
+//     <div className="furniture-item" onClick={handleItemClick}>
+//       <div
+//         className="furniture-item__img"
+//         style={{
+//           backgroundImage: `url(${imageUrl})`,
+//         }}
+//       />
+//       <div className="button-container"></div>
+//       <p className="furniture-item__category">{category}</p>
+
+//       <div className="furniture-item__details">
+//         <p className="furniture-item__name">{name}</p>
+//         <p className="furniture-item__price">{`$${price}`}</p>
+//       </div>
+
+//       {/* Pass props to ProductModal */}
+//       <ProductModal
+//         item={item}
+//         toggleModalHidden={toggleModalHidden}
+//         addItemToCart={addItem}
+//       />
+//     </div>
+//   );
+// };
+
+// const mapDispatchToProps = (dispatch) => ({
+//   toggleModalHidden: (item) => dispatch(toggleModalHidden(item)),
+//   addItem: (item) => dispatch(addItem(item)),
+// });
+
+// export default connect(null, mapDispatchToProps)(ShopItem);
+
 import React from "react";
 import { connect } from "react-redux";
 import "./shop-item.styles.scss";
 
-import { addItem } from "../../redux/cart/cart.actions";
 import { toggleModalHidden } from "../../redux/product-modal/product-modal.actions";
-import { AddedToCartModal } from "../../animations/animations";
-import ProductModal from "../product-modal/product-modal.component";
 
-const ShopItem = ({ item, category, addItem, toggleModalHidden }) => {
+const ShopItem = ({ item, category, toggleModalHidden }) => {
   const { imageUrl, name, price } = item;
 
   const handleItemClick = (e) => {
@@ -16,7 +66,8 @@ const ShopItem = ({ item, category, addItem, toggleModalHidden }) => {
     toggleModalHidden(item);
   };
 
-  console.log("Props in ShopItem:", item);
+   // Log the item prop before passing to ProductModal
+   console.log("Item in ShopItem before passing to ProductModal:", item);
 
   return (
     <div className="furniture-item" onClick={handleItemClick}>
@@ -33,20 +84,13 @@ const ShopItem = ({ item, category, addItem, toggleModalHidden }) => {
         <p className="furniture-item__name">{name}</p>
         <p className="furniture-item__price">{`$${price}`}</p>
       </div>
-
-      {/* Pass props to ProductModal */}
-      <ProductModal
-        item={item}
-        toggleModalHidden={toggleModalHidden}
-        addItemToCart={addItem}
-      />
     </div>
   );
 };
 
 const mapDispatchToProps = (dispatch) => ({
   toggleModalHidden: (item) => dispatch(toggleModalHidden(item)),
-  addItem: (item) => dispatch(addItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(ShopItem);
+
